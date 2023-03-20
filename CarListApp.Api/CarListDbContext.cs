3d78279779c9d601fcbdbab604ad41dff2ea8c70
Jose Mendez <jose.mendez@gorillalogic.com>
2023-03-20 16:48:32 -0600

    First commit

---
+++ CarListApp.Api/CarListDbContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarListApp.Api
+{
+	public class CarListDbContext : DbContext
+	{
+		public CarListDbContext(DbContextOptions<CarListDbContext> options): base(options)
+		{
+
+		}
+
+		public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Car>().HasData(
+				new Car
+				{
+					Id = 1,
+					Make = "Nissan",
+					Model = "Atlas",
+					Vin = "ABC1"
+				},
+                new Car
+                {
+                    Id = 2,
+                    Make = "Honda",
+                    Model = "Stream",
+                    Vin = "ABC2"
+                },
+                new Car
+                {
+                    Id = 3,
+                    Make = "Honda",
+                    Model = "Civid",
+                    Vin = "ABC3"
+                },
+                new Car
+                {
+                    Id = 4,
+                    Make = "Honda",
+                    Model = "Fit",
+                    Vin = "ABC4"
+                },
+                new Car
+                {
+                    Id = 5,
+                    Make = "Nissan",
+                    Model = "Murano",
+                    Vin = "ABC5"
+                },
+                new Car
+                {
+                    Id = 6,
+                    Make = "Nissan",
+                    Model = "Dualis",
+                    Vin = "ABC6"
+                },
+                new Car
+                {
+                    Id = 7,
+                    Make = "Audi",
+                    Model = "A5",
+                    Vin = "ABC7"
+                },
+                new Car
+                {
+                    Id = 8,
+                    Make = "Jaguar",
+                    Model = "F-Pace",
+                    Vin = "ABC8"
+                }
+
+            );
+        }
+    }
+}
---
+++ CarListApp.Api/Migrations/20230319224303_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarListApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Make = table.Column<string>(type: "TEXT", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", nullable: false),
+                    Vin = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Make", "Model", "Vin" },
+                values: new object[,]
+                {
+                    { 1, "Nissan", "Atlas", "ABC1" },
+                    { 2, "Honda", "Stream", "ABC2" },
+                    { 3, "Honda", "Civid", "ABC3" },
+                    { 4, "Honda", "Fit", "ABC4" },
+                    { 5, "Nissan", "Murano", "ABC5" },
+                    { 6, "Nissan", "Dualis", "ABC6" },
+                    { 7, "Audi", "A5", "ABC7" },
+                    { 8, "Jaguar", "F-Pace", "ABC8" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
